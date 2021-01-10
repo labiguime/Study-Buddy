872ed564510cb11ad9a62736cd0c9ae1b4fc622b
rafid <rafidashabpranta@gmail.com>
2021-01-09 17:39:51 -0800

    Add some initial Grid

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,28 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/Navbar';
 import DrawingBoard from 'react-drawing-board';
+import ReactPlayer from 'react-player';
+import { useState } from 'react';
+
+import Grid from '@material-ui/core/Grid'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
       <Navbar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <DrawingBoard
-          userId="user1" // identify for different players.
-        />
+      <header className="App-header"> 
+      <Grid container>
+          <Grid item xs='9'>
+            <ReactPlayer
+              url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+            />
+          </Grid>
+          <Grid item xs='3'>
+            Chat
+          </Grid>
+      </Grid>
+      <Grid container>
+          <Grid item xs='6'>
+            drawing
+          </Grid>
+          <Grid item xs='6'>
+            IDE
+          </Grid>
+      </Grid>
+      
+      {/* <DrawingBoard
+        userId="user1" // identify for different players.
+      /> */}
       </header>
     </div>
   );
