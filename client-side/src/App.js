18a9758c799aa3e1b19291e190d2be8f6733ef61
leafittome <imjamesyoung@gmail.com>
2021-01-09 16:44:20 -0800

    added navbar and whiteboard canvas

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import DrawingBoard from 'react-drawing-board';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +20,9 @@ function App() {
         >
           Learn React
         </a>
+        <DrawingBoard
+          userId="user1" // identify for different players.
+        />
       </header>
     </div>
   );
---
+++ client-side/src/components/Navbar/Navbar.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Divider from '@material-ui/core/Divider'
+
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({    
+    root: {
+        backgroundColor:  "white",
+        //backgroundColor:  "#ffdd00",
+    },
+    box: {
+        height: 35,
+        width: 250,
+        textAlign: "center",
+        display: "flex",
+      //marginTop: theme.spacing(1),    
+      marginTop: 15,
+    }, 
+    container: {
+        display: "none",
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+            position: 'relative', 
+            width: 'auto'
+          },
+      marginLeft: 'auto',
+    },
+    nav: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+          display: 'flex',
+          width: 'auto',
+          justifyContent: 'center'
+        },
+    },
+    name: {      
+        color: "black",
+        "transition" : "all 0.6s",
+        "font-size" : "26px",
+        "font-family" : "Cairo, sans-serif",
+        "letter-spacing" : "2px",
+        "text-decoration": "none",
+        marginLeft: "30px",
+        marginRight: "30px",
+        opacity : "1.0",
+            "&:hover": {
+                opacity: "0.6",
+                "font-size": "27px"
+          }
+    },
+    link: {
+        "text-decoration": "none",
+    },
+    navBtns: {
+        color: 'black',
+        "transition" : "all 0.6s",
+        "font-size" : "1rem",
+        "font-family" : "Cairo, sans-serif",
+        "letter-spacing" : "2px",
+        "text-transform" : "uppercase",
+        "text-decoration": "none",
+        opacity : "1.0",
+        "&:hover": {
+            opacity: "0.6",
+            "font-size": "1.01rem"
+      }
+    },
+    drawerPaper: {
+      width: 250,
+    },
+  }));
+  
+const Navbar = () => {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null); 
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+  
+    const handleDrawerToggle = () => {
+      setMobileOpen(!mobileOpen);
+    };
+    const open = Boolean(anchorEl);
+  
+
+    return (
+        <>
+        <AppBar className={classes.root} position="sticky" color='transparent'>
+            <Toolbar>                    
+                <Grid container>
+                    <div className={classes.title}>
+                        <a className={classes.link} href="">
+                            <Typography  className={classes.name} variant="h5">Study</Typography>
+                        </a>        
+                    </div>
+                    <Grid item>
+                        <TextField className={classes.videoURL} color="primary" variant="filled">Video URL</TextField>
+                    </Grid>
+                </Grid>
+                <Grid className={classes.nav} container>
+                    <Grid item>
+                        <List className={classes.container}>                      
+                            
+                            <ListItem>
+                                <li><a className={classes.navBtns}>Share</a></li>
+                            </ListItem>
+                            <ListItem >
+                                <li><a className={classes.navBtns}>Join</a></li>
+                            </ListItem>
+                            <ListItem>
+                                <li><a className={classes.navBtns}>User</a></li>
+                            </ListItem>
+                        </List>
+                    </Grid>
+                </Grid>
+               
+                {/* <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    className={classes.menuButton}
+                >
+                    <MenuIcon />
+                </IconButton> */}
+
+                {/* Mobile Navbar */}
+                {/* <Drawer
+                    variant="temporary"
+                    anchor="right"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    classes={{
+                    paper: classes.drawerPaper,
+                    }}
+                    ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                    }}
+                >
+                    <Paper elevation={0} className={classes.drawerPaper}>
+                    <Button size='small' onClick={handleDrawerToggle} style={{margin: 10}} color="default">
+                        <CloseIcon color='primary'/>
+                    </Button>
+                        
+                    <List>
+                        <Divider />
+                        <ListItem>
+                            <li><a className={classes.navBtns}>Home</a></li>
+                        </ListItem>
+                       
+                    </List>
+                  
+                    </Paper>
+                    
+                </Drawer> */}
+            </Toolbar>
+        </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
