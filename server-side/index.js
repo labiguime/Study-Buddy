0f65e8418b68cc7daeb956173cfbca864d3306c4
labiguime <leoabiguime@gmail.com>
2021-01-09 17:40:35 -0800

    Setup socket.io

--- server-side/index.js
+++ server-side/index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+// We setup socket.io
+require('./routes/socket')(io);
+
+// We setup express
 server.listen(3000, () => {
 	console.log('The server is up and running on port 3000!');
 });
